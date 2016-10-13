1a42010075dddde28f6d0faf4072f188023bddb8
Simone Vittori <hello@simonewebdesign.it>
2016-10-13 11:58:38 +0100

    Fix input

--- src/App.elm
+++ src/App.elm
@@ -44,7 +44,7 @@ type alias Model =
 
 initialModel : Model
 initialModel =
-    { input  = "5 3\n1 1 E\nRFRFRFRF\n3 2 N\nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL"
+    { input  = "5 3\n1 1 E\nRFRFRFRF\n\n3 2 N\nFRRFLLFFRRFLL\n\n0 3 W\nLLFFFLFLFL"
     , output = "" }
 
 
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -13,7 +13,7 @@ all =
                 let
                     ( newModel, _ ) =
                        App.update
-                           (App.ChangeInput "5 3\n1 1 E\nRFRFRFRF\n3 2 N\nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL")
+                           (App.ChangeInput "5 3\n1 1 E\nRFRFRFRF\n\n3 2 N\nFRRFLLFFRRFLL\n\n0 3 W\nLLFFFLFLFL")
                            App.initialModel
                 in
                     Expect.equal "1 1 E\n3 3 N LOST\n2 3 S" newModel.output
