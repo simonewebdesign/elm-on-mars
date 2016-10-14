da40f3f479e5d27dd1dda0bdff71771e4e44136f
Simone Vittori <hello@simonewebdesign.it>
2016-10-14 14:29:54 +0100

    Lost

--- src/App.elm
+++ src/App.elm
@@ -24,7 +24,11 @@ main =
 
 -- TYPES
 
-type alias Position = ( Int, Int, Orientation )
+type alias Grid = ( Int, Int )
+
+type alias Robot = ( Lost, Int, Int, Orientation )
+
+type alias Lost = Bool
 
 type Orientation = North | South | East | West
 
@@ -36,9 +40,9 @@ type Instruction = Left | Right | Forward
 type alias Model =
     { input  : String
     , output : List (Html Msg)
-    , robot  : Position
-    , scents : List Position
-    , grid   : ( Int, Int )
+    , robot  : Robot
+    , scents : List Robot
+    , grid   : Grid
     }
 
 
@@ -46,14 +50,14 @@ initialModel : Model
 initialModel =
     { input  = "5 3\n1 1 E\nRFRFRFRF\n\n3 2 N\nFRRFLLFFRRFLL\n\n0 3 W\nLLFFFLFLFL"
     , output = initialOutput
-    , robot  = initialPosition
+    , robot  = initialRobot
     , scents = []
     , grid   = initialSize
     }
 
 initialOutput = [text ""]
 
-initialPosition = ( 0, 0, North )
+initialRobot = ( False, 0, 0, North )
 
 initialSize = ( 0, 0 )
 
@@ -66,8 +70,8 @@ type Msg
     = NoOp
     | ChangeInput Input
     | ChangeOutput ( Input, Input )
-    | SetGrid ( ( Int, Int ), Input )
-    | SetPosition ( Position, Input )
+    | SetGrid ( Grid, Input )
+    | SetRobot ( Robot, Input )
     | ProcessInstructions ( (List Instruction), Input )
     | ProcessInstruction Instruction
 
@@ -84,7 +88,7 @@ update msg model =
             let _ = Debug.log "ChangeOutput" nextInput in
             ( { model
                 | output = model.output ++ [text s, br [] []]
-                --not sure it's necessary, robot = initialPosition
+                --not sure it's necessary, robot = initialRobot
               }
             , if String.isEmpty nextInput then
                 Cmd.none
@@ -95,14 +99,14 @@ update msg model =
         SetGrid ( coords, nextInput ) ->
             ( { model | grid = coords }, parse2ndLine nextInput )
 
-        SetPosition ( initialPos, nextInput ) ->
+        SetRobot ( initialPos, nextInput ) ->
             ( { model | robot = initialPos }, parse3rdLine nextInput )
 
         ProcessInstructions (( list, nextInput ) as instructions) ->
             case list of
                 [] ->
                     let _ = Debug.log "DONE" nextInput in
-                    update (ChangeOutput ( out model, nextInput )) model 
+                    update (ChangeOutput ( out model, nextInput )) model
 
                 instruction :: tail ->
                     let
@@ -112,7 +116,7 @@ update msg model =
 
         ProcessInstruction instruction ->
             let
-                ( x, y, z ) = model.robot
+                ( isLost, x, y, z ) = model.robot
             in
                 case instruction of
                     Left ->
@@ -124,7 +128,7 @@ update msg model =
                                     East -> North
                                     West -> South
                         in
-                            ( { model | robot = ( x, y, newOrientation ) }, Cmd.none )
+                            ( { model | robot = ( isLost, x, y, newOrientation ) }, Cmd.none )
 
                     Right ->
                         let
@@ -135,7 +139,7 @@ update msg model =
                                     East -> South
                                     West -> North
                         in
-                            ( { model | robot = ( x, y, newOrientation ) }, Cmd.none )
+                            ( { model | robot = ( isLost, x, y, newOrientation ) }, Cmd.none )
 
                     Forward ->
                         let
@@ -156,9 +160,13 @@ update msg model =
                                 newX < 0 || newY < 0
                         in
                             if outOfBounds then
-                                ( { model | scents = model.robot :: model.scents }, Cmd.none )
+                                ( { model
+                                    | robot = ( True, newX, newY, z )
+                                    , scents = model.robot :: model.scents }
+                                , Cmd.none
+                                )
                             else
-                                ( { model | robot = ( newX, newY, z ) }, Cmd.none )
+                                ( { model | robot = ( isLost, newX, newY, z ) }, Cmd.none )
 
 
 parse1stLine : String -> Cmd Msg
@@ -166,7 +174,7 @@ parse1stLine str =
     Task.perform (always NoOp) SetGrid (parseCoordsTask str)
 
 
-parseCoordsTask : String -> Task Never ( ( Int, Int ), String )
+parseCoordsTask : String -> Task Never ( Grid, String )
 parseCoordsTask str =
     Task.succeed (parseCoords str)
     |> Task.map (\result ->
@@ -175,7 +183,7 @@ parseCoordsTask str =
                 Err msg -> let _ = Debug.log "coords task failed" msg in ( initialSize, "" ))
 
 
-parseCoords : String -> Result String ( ( Int, Int ), String )
+parseCoords : String -> Result String ( Grid, String )
 parseCoords str =
     case Combine.parse coords str of
         (( Ok parsed, {input} ) as res) ->
@@ -188,20 +196,20 @@ parseCoords str =
 
 parse2ndLine : String -> Cmd Msg
 parse2ndLine str =
-    Task.perform (always NoOp) SetPosition (parsePositionTask str)
+    Task.perform (always NoOp) SetRobot (parseRobotTask str)
 
 
-parsePositionTask : String -> Task Never ( Position, String )
-parsePositionTask str =
-    Task.succeed (parsePosition str)
+parseRobotTask : String -> Task Never ( Robot, String )
+parseRobotTask str =
+    Task.succeed (parseRobot str)
     |> Task.map (\result ->
             case result of
                 Ok res -> res
-                Err msg -> let _ = Debug.log "position task failed" msg in ( initialPosition, "" ))
+                Err msg -> let _ = Debug.log "position task failed" msg in ( initialRobot, "" ))
 
 
-parsePosition : String -> Result String ( Position, String )
-parsePosition str =
+parseRobot : String -> Result String ( Robot, String )
+parseRobot str =
      case Combine.parse position str of
         ( Ok parsed, {input} ) ->
             Ok ( parsed, input )
@@ -242,7 +250,7 @@ setOutput model input =
 newOutput : Model -> Input -> Task Never ( String, Input )
 newOutput model input =
     let
-        ( x, y, z ) = model.robot
+        ( _, x, y, z ) = model.robot
         _ = Debug.log "newOutput robot" model.robot
     in
         (Task.succeed <| out model)
@@ -252,16 +260,13 @@ newOutput model input =
 out : Model -> String
 out model =
     let
-        ( x, y, z ) = model.robot
+        _ = Debug.log "OUT" model
+        ( isLost, x, y, z ) = model.robot
+        ( gridX, gridY ) = model.grid
     in
         toString x ++ " " ++ toString y ++ " "
         ++ String.fromChar (toCharOrientation z)
-
-
---parseNext : String -> Cmd Msg
---parseNext str =
---    let _ = Debug.log "parseNext" str in
---    Task.perform (always NoOp) ChangeOutput (Task.succeed ("str", "str"))
+        ++ if isLost then " LOST" else ""
 
 
 -- VIEW
@@ -277,14 +282,14 @@ view ({input, output} as model) =
 
 -- PARSING
 
-coords : Parser ( Int, Int )
+coords : Parser Grid
 coords =
     (,) <$> int <* space <*> int <* newline
 
 
-position : Parser Position
+position : Parser Robot
 position =
-    (,,) <$> int <* space <*> int <* space <*> orientation <* newline
+    (,,,) False <$> int <* space <*> int <* space <*> orientation <* newline
 
 
 orientation : Parser Orientation
