e43d04a8bc028158189b14c2b661800dfdf44163
Simone Vittori <hello@simonewebdesign.it>
2016-10-19 01:29:52 +0100

    Re-enable tests

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -4,20 +4,21 @@ import Test exposing (..)
 import Expect
 import App
 
-input = "5 3\n1 1 E\nRFRFRFRF\n\n3 2 N\nFRRFLLFFRRFLL\n\n0 3 W\nLLFFFLFLFL\n\n"
-output = ["1 1 E", "3 3 N LOST", "2 3 S"]
 
 all : Test
 all =
     describe "A Martian Test Suite"
-        [
-          --test "input output" <|
-          --  \() ->
-          --      let
-          --          ( newModel, _ ) =
-          --             App.update
-          --                 (App.ChangeInput input)
-          --                 newModel
-          --      in
-          --          Expect.equal output newModel.output
+        [ test "end-to-end test #1" <|
+            \() ->
+                let newModel = App.update (App.Change input) App.initialModel
+                    input = "5 3\n1 1 E\nRFRFRFRF\n\n3 2 N\nFRRFLLFFRRFLL\n\n0 3 W\nLLFFFLFLFL\n"
+                    output = ["1 1 E", "3 3 N LOST", "2 3 S"]
+                in Expect.equal output newModel.output
+
+        , test "end-to-end test #2" <|
+            \() ->
+                let newModel = App.update (App.Change input) App.initialModel
+                    input = "15 25\n12 6 E\nFFRLLFLFFFFFFFFFFFFFFF\n"
+                    output = ["-1 7 W LOST"]
+                in Expect.equal output newModel.output
         ]
