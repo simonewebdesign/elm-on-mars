0cb5bd752af3bcc59de45e3ed92db441a8552794
Simone Vittori <hello@simonewebdesign.it>
2016-10-19 01:00:55 +0100

    Move parsers in a separate module

--- src/App.elm
+++ src/App.elm
@@ -4,12 +4,6 @@ import Html exposing (Html, div, textarea, p, text, br)
 import Html.Attributes exposing (rows)
 import Html.App as Html
 import Html.Events exposing (onInput)
-
-import Combine exposing (Parser, manyTill, sepBy1)
-import Combine.Infix exposing (..)
-import Combine.Char exposing (newline, space, oneOf, eol)
-import Combine.Num exposing (int)
-
 import Types exposing (..)
 
 main : Program Never
@@ -99,13 +93,6 @@ handleInput model newInput result =
                 { model | input = newInput, output = newOutput }
 
 
-parse : String -> Result String ProgramInput
-parse str =
-    case Combine.parse programInput str of
-        ( Ok parsed, _ ) -> Ok parsed
-        ( Err msg, _ ) -> Err <| "parse error: " ++ toString msg
-
-
 turn : Instruction -> Orientation -> Orientation
 turn instruction orientation =
     case instruction of
@@ -140,66 +127,3 @@ view {input, output} =
         [ textarea [ onInput Change, rows 20 ] [ text input ]
         , p [] <| List.intersperse (br[][]) (List.map text output)
         ]
-
-
--- PARSING
-
-programInput : Parser ProgramInput
-programInput =
-    (,) <$> grid <* newline
-        <*> sepBy1 newline pair
-
-
-pair : Parser ( Robot, Instructions )
-pair =
-    (,) <$> position <* newline
-        <*> instructions
-
-
-grid : Parser Grid
-grid =
-    (,) <$> int <* space <*> int
-
-
-position : Parser Robot
-position =
-    (,,,) False <$> int <* space <*> int <* space <*> orientation
-
-
-orientation : Parser Orientation
-orientation =
-    toOrientation <$> oneOf ['N', 'S', 'E', 'W']
-
-toOrientation : Char -> Orientation
-toOrientation c =
-    case c of
-        'N' -> North
-        'S' -> South
-        'E' -> East
-        'W' -> West
-        _ -> Debug.crash "wat"
-
-toStringOrientation : Orientation -> String
-toStringOrientation i =
-    case i of
-        North -> "N"
-        South -> "S"
-        East -> "E"
-        West -> "W"
-
-
-instructions : Parser Instructions
-instructions =
-    manyTill instruction (newline <|> eol)
-
-instruction : Parser Instruction
-instruction =
-    toInstruction <$> oneOf ['L', 'R', 'F']
-
-toInstruction : Char -> Instruction
-toInstruction c =
-    case c of
-        'L' -> Left
-        'R' -> Right
-        'F' -> Forward
-        _ -> Debug.crash "wat"
